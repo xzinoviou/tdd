f3059d4f0c178252f850b3af538300d12efceb47
xzinoviou <xzinoviou@gmail.com>
2019-08-28 23:02:28 +0300

    added chapter 14 - reducing currencies

--- src/main/java/com/xzinoviou/tdd/domain/Bank.java
+++ src/main/java/com/xzinoviou/tdd/domain/Bank.java
@@ -1,9 +1,23 @@
 package com.xzinoviou.tdd.domain;
 
+import java.util.Hashtable;
+
 /** @author xzinoviou created 27/8/19 */
 public class Bank {
 
+  private Hashtable rates = new Hashtable();
+
   Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
+  }
+
+  int rate(String from, String to) {
+    if (from.equals(to)) return 1;
+    Integer rate = (Integer) rates.get(new Pair(from, to));
+    return rate.intValue();
+  }
+
+  void addRate(String from, String to, int rate) {
+    rates.put(new Pair(from, to), rate);
   }
 }
--- src/main/java/com/xzinoviou/tdd/domain/Expression.java
+++ src/main/java/com/xzinoviou/tdd/domain/Expression.java
@@ -1,10 +1,7 @@
 package com.xzinoviou.tdd.domain;
 
-/**
- * @author xzinoviou
- * created 27/8/19
- */
+/** @author xzinoviou created 27/8/19 */
 public interface Expression {
 
-    Money reduce(String to);
+  Money reduce(Bank bank, String to);
 }

5ec482f451436927e61cea371512aa69beda7e65
xzinoviou <xzinoviou@gmail.com>
2019-08-29 08:40:44 +0300

    added chapter 16 - finishing

--- src/test/java/com/xzinoviou/tdd/domain/DollarTest.java
+++ src/test/java/com/xzinoviou/tdd/domain/DollarTest.java
@@ -73,18 +73,6 @@ public class DollarTest {
     assertEquals(1, new Bank().rate("USD", "USD"));
   }
 
-  /** Initial test */
-  /*
-    @Test
-    public void testMixedAddition() {
-      Expression fiveBucks = Money.dollar(5);
-      Expression tenFrancs = Money.franc(10);
-      Bank bank = new Bank();
-      bank.addRate("CHF", "USD", 2);
-      Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
-      assertEquals(Money.dollar(10), result);
-    }
-  */
   @Test
   public void testMixedAddition() {
     Expression fiveBucks = Money.dollar(5);
@@ -93,5 +81,27 @@ public class DollarTest {
     bank.addRate("CHF", "USD", 2);
     Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
     assertEquals(Money.dollar(10), result);
+  }
+
+  @Test
+  public void testSumPlusMoney() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(15), result);
+  }
+
+  @Test
+  public void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(20), result);
   }
 }
